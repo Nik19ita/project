0318feee99e02dc61d919c02e529ad237207eb9b
Nikita Muratov <muratov.nickita@gmail.com>
2025-05-31 15:22:19 +0300

    "remove padding for mobile"

--- src/widgets/ListCards/ListCards.jsx
+++ src/widgets/ListCards/ListCards.jsx
@@ -5,7 +5,7 @@ const ListCards = () => {
   return (
     <div>
       <ul
-        className="columns-1 sm:columns-2 gap-x-[20px] w-fit mx-auto py-[100px] px-[15px]"
+        className="mx-auto w-fit columns-1 gap-x-[20px] py-[100px] sm:columns-2 sm:px-[15px]"
         style={{
           pageBreakInside: "avoid",
           breakInside: "avoid",
